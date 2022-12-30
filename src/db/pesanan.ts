ae031371869be8ed11862850d9be1af49b191d5c
andiputraw <andiputraw925@gmail.com>
2022-12-30 09:25:27 +0700

    add get and delete route

--- src/db/pesanan.ts
+++ src/db/pesanan.ts
@@ -1,4 +1,4 @@
-import mongoose, { Schema } from "mongoose";
+import mongoose, { mongo, ObjectId, Schema } from "mongoose";
 import { getHarga } from "./daftarMakanan";
 
 export interface Ipesanan {
@@ -29,11 +29,57 @@ export async function insertPesanan(pemesan: string, jenisPesanan: string, jumla
     const harga = await getHarga(jenisPesanan).then((result) => result * jumlah);
     //* Masukan ke daftar makanan, kembalikan sukses
     await pesanan.insertMany([{ nama: pemesan, jenisPesanan: jenisPesanan, jumlah: jumlah, totalHarga: harga }]);
-    return "Success";
+    return "ok";
   } catch (error) {
     // TODO buat logging
     console.log(error);
     //* Jika gagal, artinya ada masalah di server
+    throw "Internal Error";
+  }
+}
+
+export async function getPesanan(): Promise<Ipesanan[]> {
+  try {
+    const result = await pesanan.find();
+
+    return result;
+  } catch (error) {
+    throw "internal Error";
+  }
+}
+
+export async function getPesananById(id: string): Promise<{ status: number; msg: string; data: Ipesanan | null }> {
+  try {
+    if (!mongoose.isValidObjectId(id)) {
+      return { status: 0, msg: "Id Tidak Valid", data: null };
+    }
+
+    const result = await pesanan.findById(id);
+
+    if (result === null) {
+      return { status: 0, msg: "Id Tidak Ditemukan", data: null };
+    }
+
+    return { status: 1, msg: "ok", data: result };
+  } catch (error) {
+    throw "internal Error";
+  }
+}
+
+export async function deletePesananById(id: string): Promise<{ status: number; msg: string }> {
+  try {
+    if (!mongoose.isValidObjectId(id)) {
+      return { status: 0, msg: "Id Tidak Valid" };
+    }
+
+    const result = await pesanan.deleteOne({ _id: id });
+
+    if (result.deletedCount < 1) {
+      return { status: 0, msg: "Id Tidak Ditemukan" };
+    }
+
+    return { status: 1, msg: "ok" };
+  } catch (error) {
     throw "Internal Error";
   }
 }
--- src/services/pesanan.ts
+++ src/services/pesanan.ts
@@ -1,5 +1,5 @@
 import { Request, Response, NextFunction, Router } from "express";
-import { insertPesanan, Ipesanan, Status } from "../db/pesanan";
+import { insertPesanan, Ipesanan, Status, getPesanan, getPesananById, deletePesananById } from "../db/pesanan";
 import { body, validationResult, oneOf } from "express-validator";
 import { getDaftarMakanan } from "../db/daftarMakanan";
 
@@ -14,8 +14,29 @@ type dummy = {
 
 const tumbal: Array<dummy> = [{ nama: "andi", pesanan: "Nasi Goreng", hargaBeli: 2000 }];
 
-pesanan.get("/", (req: Request, res: Response, next: NextFunction) => {
-  res.send({ status: 200, data: tumbal });
+//* Method Get
+pesanan.get("/", async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const result = await getPesanan();
+
+    res.status(200).send({ status: 200, msg: "ok", data: result });
+  } catch (error) {
+    res.status(500).send({ status: 500, msg: error, data: null });
+  }
+});
+
+pesanan.get("/:id", async (req: Request, res: Response) => {
+  try {
+    const result = await getPesananById(req.params.id);
+
+    if (result.status === 0) {
+      return res.status(404).send({ status: 404, msg: result.msg, data: null });
+    }
+
+    res.status(200).send({ status: 200, msg: "ok", data: result });
+  } catch (error) {
+    res.status(500).send({ status: 500, msg: error, data: null });
+  }
 });
 
 //* Method POST
@@ -52,8 +73,16 @@ pesanan.post(
   }
 );
 
-pesanan.delete("/:id", (req: Request, res: Response, Next: NextFunction) => {
-  res.send(req.body);
+pesanan.delete("/:id", async (req: Request, res: Response, Next: NextFunction) => {
+  try {
+    const result = await deletePesananById(req.params.id);
+    if (result.status === 0) {
+      return res.status(404).send({ status: 404, msg: result.msg });
+    }
+    res.status(200).send({ status: 200, msg: "ok" });
+  } catch (error) {
+    res.status(500).send({ status: 500, msg: error });
+  }
 });
 
 pesanan.put("/:id", (req: Request, res: Response, Next: NextFunction) => {
